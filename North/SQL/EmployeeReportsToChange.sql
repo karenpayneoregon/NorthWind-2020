2d6e2828623ba28122db777fb5bdb98654eeb897
Karen Payne <paynekaren@comcast.net>
2020-07-14 14:07:00 -0700

    SQL statements

---
+++ North/SQL/EmployeeReportsToChange.sql
@@ -0,0 +1,5 @@
+﻿-- The original database did not have this, it's needed to get ReportsTo
+-- no need to run this
+
+ALTER TABLE [dbo].[Employees]  WITH NOCHECK ADD  CONSTRAINT [FK_Employees_Employees] FOREIGN KEY([ReportsTo])
+REFERENCES [dbo].[Employees] ([EmployeeID])
---
+++ North/SQL/EmployeesReportsToSelect.sql
@@ -0,0 +1,7 @@
+﻿
+SELECT Employees.FirstName, 
+       Employees.LastName, 
+       manager.FirstName + ' ' + manager.LastName AS Manager
+FROM Employees
+     LEFT OUTER JOIN Employees AS manager ON Employees.ReportsTo = manager.EmployeeID
+WHERE Employees.ReportsTo IS NOT NULL;
