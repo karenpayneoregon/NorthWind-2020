4a77f851bc38f4bab6c72378d83b4e5ea52cf8d6
Karen Payne <paynekaren@comcast.net>
2020-07-17 07:37:58 -0700

    Added test connection

---
+++ North/Classes/Helpers/ConnectionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Configuration.ConfigurationManager;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace North.Classes.Helpers
+{
+    public class ConnectionHelper
+    {
+        public static Exception Exception;
+        public static async Task<bool> TestConnection()
+        {
+            var connectionString =
+                $"Data Source={AppSettings["DatabaseServer"]};" +
+                $"Initial Catalog={AppSettings["NorthWinCatalog"]};" +
+                "Integrated Security=True";
+
+
+            return await Task.Run(async () =>
+            {
+                using (var cn = new SqlConnection { ConnectionString = connectionString })
+                {
+                    try
+                    {
+                        await cn.OpenAsync();
+                        return true;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Exception = ex;
+                        return false;
+                    }
+                }
+
+            });
+        }
+    }
+}
