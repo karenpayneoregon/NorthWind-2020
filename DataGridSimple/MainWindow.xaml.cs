dc8de691f34474189d45aabb417dae6ab2973186
Karen Payne <paynekaren@comcast.net>
2020-08-18 14:24:50 -0700

    Added simple example for updating a DataGrid in WPF

---
+++ DataGridSimple/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using NorthStockConfiguration;
+using NorthStockConfiguration.Classes;
+using static System.Windows.WindowStartupLocation;
+
+namespace DataGridSimple
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = CenterScreen;
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var employeeList = await EmployeeOperations.GetEmployeeListAsync();
+            EmployeeGrid.ItemsSource = employeeList;
+        }
+
+        private void ButtonCurrent_OnClick(object sender, RoutedEventArgs e)
+        {
+            var employee = EmployeeGrid.SelectedItem as Employees;
+            if (!EmployeeOperations.Update(employee))
+            {
+                MessageBox.Show("Update failed");
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ NorthStockConfiguration/Classes/EmployeeOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NorthStockConfiguration.Classes
+{
+    public class EmployeeOperations
+    {
+        /// <summary>
+        /// Get all employees
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<List<Employees>> GetEmployeeListAsync()
+        {
+            return await Task.Run(async () =>
+                {
+
+                    using (var context = new NorthwindContext())
+                    {
+                        return await context.Employees.AsNoTracking()
+                            .ToListAsync();
+                    }
+                }
+            );
+        }
+        /// <summary>
+        /// Update a single employee
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public static bool Update(Employees employee)
+        {
+            using (var context = new NorthwindContext())
+            {
+                context.Attach(employee).State = EntityState.Modified;
+                return context.SaveChanges() == 1;
+            }
+        }
+    }
+}
