72c4375b0290af4784bd55c12e869c44827ae8b1
Karen Payne <Karen.1.Payne@oregon.gov>
2020-08-30 19:17:32 -0700

    Added snum generator class project

---
+++ EnumGeneratorLibrary/Categories.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace EnumGeneratorLibrary
+{
+    /// <summary>
+    /// Categories auto generated enumeration
+    /// </summary>
+    [GeneratedCode("TextTemplatingFileGenerator", "10")]
+    public enum Categories
+    {
+      
+		/// <summary>
+        /// Beverages 
+        /// </summary>
+        Beverages = 1,
+
+      
+		/// <summary>
+        /// Condiments 
+        /// </summary>
+        Condiments = 2,
+
+      
+		/// <summary>
+        /// Confections 
+        /// </summary>
+        Confections = 3,
+
+      
+		/// <summary>
+        /// Dairy Products 
+        /// </summary>
+        DairyProducts = 4,
+
+      
+		/// <summary>
+        /// Grains/Cereals 
+        /// </summary>
+        GrainsCereals = 5,
+
+      
+		/// <summary>
+        /// Meat/Poultry 
+        /// </summary>
+        MeatPoultry = 6,
+
+      
+		/// <summary>
+        /// Produce 
+        /// </summary>
+        Produce = 7,
+
+      
+		/// <summary>
+        /// Seafood 
+        /// </summary>
+        Seafood = 8,
+
+      
+		/// <summary>
+        /// Wine 
+        /// </summary>
+        Wine = 9
+  }
+}
---
+++ EnumGeneratorLibrary/readme.md
@@ -0,0 +1,5 @@
+﻿Code to create enum from SQL-Server reference table.
+
+For instructions see Microsoft TechNet article [Entity Framework database/Code First Enum support](https://social.technet.microsoft.com/wiki/contents/articles/53169.entity-framework-databasecode-first-enum-support.aspx#Creating_Enum_for_reference_table).
+
+[GitHub repository](https://github.com/karenpayneoregon/EntityFrameworkEnum) for above article.
